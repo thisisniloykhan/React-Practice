f657a229aaa557cfefd88d21239cda697c6a829a
Niloy Khan <niloykhan.dev@gmail.com>
2026-07-19 22:08:55 +0600

    finished travel journal section

--- React-Scrimba/Data-Driven-React-challenges/20. Map quiz/quiz.md
+++ React-Scrimba/Data-Driven-React-challenges/20. Map quiz/quiz.md
@@ -1,11 +1,14 @@
 1. What does the `.map()` array method do?
 
-
+map method iterates over an array and returns a new array with the same number of items and given condition
 
 2. What do we usually use `.map()` for in React?
 
+we use map method to map a component
 
 
 3. Critical thinking: why is using `.map()` better than just
    creating the components manually by typing them out?
+
+without using map method we have to put multiple data in each component which takes a lot of time
 
--- React-Scrimba/data-driven-react/travel-journal/src/components/Entry.jsx
+++ React-Scrimba/data-driven-react/travel-journal/src/components/Entry.jsx
@@ -1,25 +1,28 @@
 import marker from "../assets/marker.png";
 
 export default function Entry(props) {
+  console.log(props);
   return (
-    <>
-      <article className="entry">
-        <div className="entry-img-container">
-          <img src={props.img.src} alt={props.img.alt} className="entry-img" />
+    <article className="entry">
+      <div className="entry-img-container">
+        <img
+          src={props.img.src}
+          alt={props.img.alt}
+          className="entry-img"
+        />
+      </div>
+      <div className="entry-body">
+        <div className="entry-header">
+          <img src={marker} alt="marker-icon" />
+          <span>{props.country.toUpperCase()}</span>
+          <a href={props.googleMapsLink}>View on Google Maps</a>
         </div>
-        <div className="entry-body">
-          <div className="entry-header">
-            <img src={marker} alt="marker-icon" />
-            <span>{props.country.toUpperCase()}</span>
-            <a href={props.googleMapsLink}>View on Google Maps</a>
-          </div>
-          <div className="entry-info">
-            <h2>{props.title}</h2>
-            <h5>{props.dates}</h5>
-            <p>{props.text}</p>
-          </div>
+        <div className="entry-info">
+          <h2>{props.title}</h2>
+          <h5>{props.dates}</h5>
+          <p>{props.text}</p>
         </div>
-      </article>
-    </>
+      </div>
+    </article>
   );
 }
