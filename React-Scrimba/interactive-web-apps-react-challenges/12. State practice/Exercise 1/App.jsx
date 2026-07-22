d1c1a53775e3bd7afc51915e9b39863b615ce769
Niloy Khan <niloykhan.dev@gmail.com>
2026-07-22 22:57:56 +0600

    learning about updating the state

--- React-Scrimba/interactive-web-apps-react-challenges/12. State practice/Exercise 1/App.jsx
+++ React-Scrimba/interactive-web-apps-react-challenges/12. State practice/Exercise 1/App.jsx
@@ -1,19 +1,33 @@
-import React from "react"
+import React from "react";
 
 export default function App() {
-    /**
-     * Challenge: 
-     * Create state to track our count value (initial value is 0)
-     * Don't forget to replace the hard-coded "0" with your new state
-     */
-    return (
-        <main className="container">
-            <h1>How many times will Bob say "state" in this section?</h1>
-            <div className="counter">
-                <button className="minus" aria-label="Decrease count">–</button>
-                <h2 className="count">0</h2>
-                <button className="plus" aria-label="Increase count">+</button>
-            </div>
-        </main>
-    )
+  /**
+   * Challenge:
+   * Create state to track our count value (initial value is 0)
+   * Don't forget to replace the hard-coded "0" with your new state
+   */
+  const [count, setCount] = React.useState(0);
+
+  function add() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  function minus() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  return (
+    <main className="container">
+      <h1>How many times will Bob say "state" in this section?</h1>
+      <div className="counter">
+        <button className="minus" aria-label="Decrease count" onClick={minus}>
+          –
+        </button>
+        <h2 className="count">{count}</h2>
+        <button className="plus" aria-label="Increase count" onClick={add}>
+          +
+        </button>
+      </div>
+    </main>
+  );
 }
--- React-Scrimba/interactive-web-apps-react-challenges/14. Changing state quiz/quiz.md
+++ React-Scrimba/interactive-web-apps-react-challenges/14. Changing state quiz/quiz.md
@@ -1,13 +1,14 @@
 1. You have 2 options for what you can pass in to a
    state setter function (e.g. `setCount`). What are they?
-   
-
+   1. directly setting the value or
+   2. Pass in a callback function that takes the old value of the count the return the new value according to the condition
 
 2. When would you want to pass the first option (from answer
    above) to the state setter function?
 
-
+if i want to directly update the state and we don't care about the old value
 
 3. When would you want to pass the second option (from answer
    above) to the state setter function?
 
+if i want to work with the prev value of the state and care about the old value
